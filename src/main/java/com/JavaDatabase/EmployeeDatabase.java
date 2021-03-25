ec09f48c24c13e87739993b786b23aa6a13eac4a
MayurMansukh <manojmansukh7004>
2021-03-25 13:28:31 +0530

    add logic to show PayrollService table data

--- src/main/java/com/JavaDatabase/EmployeeDatabase.java
+++ src/main/java/com/JavaDatabase/EmployeeDatabase.java
@@ -1,6 +1,10 @@
 package com.JavaDatabase;
 
+import javax.xml.crypto.Data;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeeDatabase {
 
     private Connection getConnection() throws IllegalAccessException {
@@ -27,6 +31,41 @@ public class EmployeeDatabase {
 
     }
 
+    public List<PayrollServiceData> readData() {
+        String Sql_Query = "select * from PayrollServiceTable";
+        List<PayrollServiceData> payrollServiceData = new ArrayList<>();
+        try {
+            Connection connection = this.getConnection();
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(Sql_Query);
+
+            while (resultSet.next()) {
+
+                int id = resultSet.getInt(1);
+                String Name = resultSet.getString(2);
+                String StartDate = resultSet.getString(3);
+                int Salary = resultSet.getInt(4);
+
+                System.out.println();
+                System.out.println("id=" + id);
+                System.out.println("Name=" + Name);
+                System.out.println("StartDate=" + StartDate);
+                System.out.println("Salary=" + Salary);
+
+                PayrollServiceData payrollServiceData1 = new PayrollServiceData(resultSet.getInt(1), resultSet.getString(2), resultSet.getString(3), resultSet.getInt(4));
+                payrollServiceData.add(payrollServiceData1);
+
+
+            }
+            statement.close();
+            connection.close();
+
+
+        } catch (SQLException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return payrollServiceData;
+    }
 
 
 
