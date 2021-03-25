8ef1ac9ae0340b26906b4e0e30c6e05a9ef346ca
MayurMansukh <manojmansukh7004>
2021-03-25 13:25:22 +0530

    add logic to connection to PayrollService Database

---
+++ src/main/java/com/JavaDatabase/EmployeeDatabase.java
@@ -0,0 +1,36 @@
+package com.JavaDatabase;
+
+import java.sql.*;
+public class EmployeeDatabase {
+
+    private Connection getConnection() throws IllegalAccessException {
+        String JDBCURL = "jdbc:mysql://localhost:3306/payroll_service?useSSL=false";
+        String UserName = "root";
+        String Password = "P@ssw0rd1@2";
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Dreiver loaded");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalAccessException(String.format("Driver not found in classpath%s", e));
+
+        }
+        try {
+            System.out.println("Connecting to database" + JDBCURL);
+            connection = DriverManager.getConnection(JDBCURL, UserName, Password);
+            System.out.println("Connection succesfully" + connection);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return connection;
+
+    }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/com/JavaDatabase/EmployeeDatabaseTest.java
@@ -0,0 +1,10 @@
+package com.JavaDatabase;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+class EmployeeDatabaseTest {
+
+
+}
